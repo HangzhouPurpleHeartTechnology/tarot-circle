7403d30049fc70fd02275757f28bb59a6e8abb91
Yizhe Wang <yizhe87@gmail.com>
2021-02-16 11:26:11 -0800

    create fold card component

---
+++ frontend/src/components/cards/DrawCards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function DrawCards() {
+  
+  return <div>
+
+  </div>;
+}
+
+export default DrawCards;
---
+++ frontend/src/components/cards/FoldCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+const background =
+  'https://res.cloudinary.com/willwang/image/upload/v1613457487/cardback_ikgn08.png';
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+function FoldCard() {
+  return (
+    <div className={classes.root}>
+      <img src={background} alt='card back' />
+    </div>
+  );
+}
+
+export default FoldCard;
